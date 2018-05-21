f69b85ed8059b89f92c5aa587f25551ca5bc50ee
Joseph Lewis III <jlewisiii@google.com>
2018-05-21 07:51:29 -0700

    added blobs and clobs
    Change-Id: Ibd3cafc0e2c6e78d93fecf4938a9f1b34d8a6dae

---
+++ beater/codec/blob_test.go
@@ -0,0 +1,96 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package codec
+
+import (
+	"strings"
+
+	"testing"
+)
+
+func TestBlobCodecNextErr(t *testing.T) {
+	cases := map[string]struct {
+		Data      string
+		Length    int
+		ExpectErr bool
+	}{
+		"empty file": {
+			Data:      ``,
+			Length:    1,
+			ExpectErr: false,
+		},
+		"single line": {
+			Data:      `foo`,
+			Length:    1,
+			ExpectErr: false,
+		},
+		"two lines": {
+			Data:      "foo\nbar",
+			Length:    1,
+			ExpectErr: false,
+		},
+	}
+
+	for tn, tc := range cases {
+		reader := strings.NewReader(tc.Data)
+		c := NewBlobCodec("testfile", reader)
+
+		counter := 0
+		for c.Next() {
+			counter++
+		}
+
+		hasErr := c.Err() != nil
+		if hasErr != tc.ExpectErr {
+			t.Errorf("%q | Got error %d, expected? %v", tn, c.Err(), tc.ExpectErr)
+		}
+
+		if counter != tc.Length {
+			t.Errorf("%q | Expected to decode %d objects, got %d", tn, tc.Length, counter)
+		}
+	}
+}
+
+func TestBlobValFunctionality(t *testing.T) {
+	data := "foo\nbar \nba zz"
+	expected := "Zm9vCmJhciAKYmEgeno="
+	
+	reader := strings.NewReader(data)
+	codec := NewBlobCodec("testfile", reader)
+
+	if !codec.Next() {
+		t.Error("Quit too early.")
+	}
+
+	val := codec.Value()
+
+	// lines are indexed at 1
+	if val["line"] != 1 {
+		t.Errorf("Expected line to be %d, got %d", val["line"], 1)
+	}
+
+	if val["file"] != "testfile" {
+		t.Errorf("Expected file to be 'testfile', got %q", val["file"])
+	}
+
+	if val["event"] != expected {
+		t.Errorf("Expected event to be %q, got %q", expected, val["event"])
+	}
+	
+	if codec.Next() {
+		t.Error("Should only be able to call next once")
+	}
+
+}
---
+++ beater/codec/clob.go
@@ -0,0 +1,65 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package codec
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/elastic/beats/libbeat/common"
+)
+
+func NewClobCodec(path string, input io.Reader) Codec {
+
+	bytes, err := ioutil.ReadAll(input)
+	text := ""
+	if err == nil {
+		text = string(bytes)
+	}
+	
+	event := common.MapStr{
+		"event": text,
+		"file": path,
+		"line": 1,
+	}
+
+	return &ClobCodec{
+		event:   event,
+		err:     err,
+		hasMore: err == nil,
+	}
+}
+
+// BufioCodec is a basic codec that reads a file line by line and reports the contents
+// line number and which file the line came from
+type ClobCodec struct {
+	event   common.MapStr
+	err		error
+	hasMore	bool
+}
+
+func (codec *ClobCodec) Next() bool {
+	orig := codec.hasMore
+	codec.hasMore = false
+	return orig
+}
+
+func (codec *ClobCodec) Value() common.MapStr {
+	return codec.event
+}
+
+func (codec *ClobCodec) Err() error {
+	return codec.err
+}
